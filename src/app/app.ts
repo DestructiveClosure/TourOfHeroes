071656197ce204b8d96142176c880323eb448f5d
tony <tl05@myscc.ca>
2026-09-15 13:03:00 -0400

    Add Input FormControl Functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Heroes } from './heroes/heroes';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Heroes],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
