eda6252f7efc6515e266e3e90e1f2270655e0218
tony <tl05@myscc.ca>
2026-09-15 12:26:24 -0400

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Tour of Heroes');
+  protected readonly title = signal('Tour of Heroes')
 }
